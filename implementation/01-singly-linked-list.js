7e8432dae8f4acd89bc88cb9c022dfe8525880dd
Antonio <antonioramirez9614@gmail.com>
2023-11-30 16:05:08 -0600

    Singly-linked-list.js

--- implementation/01-singly-linked-list.js
+++ implementation/01-singly-linked-list.js
@@ -14,63 +14,101 @@ class SinglyLinkedList {
 
     addToHead(val) {
         // Add node of val to head of linked list
-
-        // Your code here 
-
+        const newNode = new SinglyLinkedNode(val);
+        // Your code here
+        newNode.next = this.head
+        this.head = newNode
+        this.length++
+        return this
+        // console.log(this.head)
         // Write your hypothesis on the time complexity of this method here
+        // O(n)
     }
 
     addToTail(val) {
         // There are bugs in this method! Fix them!!!
 
         // Add node of val to tail of linked list
-        let newNode = new SinglyLinkedNode(data);
+        let newNode = new SinglyLinkedNode(val);
 
-        if (!head) {
-            head = newNode;
-            return head;
+        if (!this.head) {
+            this.head = newNode;
+            this.length++
+            return this;
         }
 
-        let curr = head;
-        while (curr) {
-            curr = current.next;
+        let curr = this.head;
+        while (curr.next) {
+            curr = curr.next;
         }
         curr.next = newNode;
-
-        return head;
+        this.length++
+        // console.log(this)
+        return this;
 
         // Write your hypothesis on the time complexity of this method here
     }
 
     removeFromHead() {
         // Remove node at head
+        // Your code here
+    // Should return undefined if the list is empty
+        if(!this.head) return undefined
 
-        // Your code here 
-
+    const removed = this.head
+    // Should remove head node from the list by reassigning the head pointer to the next node
+    this.head = this.head.next
+    // Should properly update the length property after removing the head node
+    this.length--
+    // Should return the removed head node when removeFromHead is called
+    return removed
         // Write your hypothesis on the time complexity of this method here
     }
 
     removeFromTail() {
-        // Remove node at tail
-
-        // Your code here 
-
+        // if there is no head, return undefined
+    if(!this.head) return undefined
+    // giving something for "curr" to point at
+    let curr = this.head
+    // assign "previous" to null
+    let previous = null
+    // loop through this
+        while(curr.next){
+            // assign previous to the current position
+            previous = curr
+            // assign current position to the next position
+            curr = curr.next
+        }
+        // if there's no prior position assign head to Null, and if not, the current position is Null
+        if(!previous){
+            this.head = null
+        } else {
+            previous.next = null
+        }
+        // decriment length
+        this.length--;
+        // return removed tail
+        return curr;
         // Write your hypothesis on the time complexity of this method here
     }
 
     peekAtHead() {
         // Return the value of head node
-
-        // Your code here 
+        if(this.length === 0) return undefined
+        // Your code here
+        return this.head.value
 
         // Write your hypothesis on the time complexity of this method here
     }
 
     print() {
         // Print out the linked list
-
-        // Your code here 
-
+        if(!this.head) return undefined
+        let curr = this.head;
+        while(curr){
+            console.log(curr.value);
+            curr = curr.next
+        }
         // Write your hypothesis on the time complexity of this method here
     }
 }
